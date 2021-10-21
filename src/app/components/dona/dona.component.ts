bb8113594508e54f217256f26b16d6b703495dd3
Cristhian Barahona <cristhian@rhinorealtyinc.com>
2021-10-21 15:27:17 -0600

    Inputs y Outputs

---
+++ src/app/components/dona/dona.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { MultiDataSet, Label, Color} from 'ng2-charts';
+
+@Component({
+  selector: 'app-dona',
+  templateUrl: './dona.component.html',
+  styles: [
+  ]
+})
+export class DonaComponent  {
+
+  @Input() title: string = 'Sin Titulo';
+
+  @Input('labels') doughnutChartLabels: Label[] = [
+    'Label1',
+    'label2',
+    'label3',
+  ];
+
+  @Input('data') doughnutChartData: MultiDataSet = [[350, 450, 100]];
+
+  public colors: Color[] = [
+    {backgroundColor: ['#6857e6', '#009fee', '#f02059']}
+  ]
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-import { PagesComponent } from './pages.component';
-import { Grafica1Component } from './grafica1/grafica1.component';
-import { ProgressComponent } from './progress/progress.component';
+//modulos
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
 
 @NgModule({
   declarations: [
@@ -23,8 +27,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     CommonModule,
+    FormsModule ,
     SharedModule,
-    RouterModule 
+    RouterModule,
+    ComponentsModule
   ]
 })
 export class PagesModule { }
